5cf85799604c9ac53cdab9e0636e219eb4df32fa
Susumu-Fukutome <tomex.beta@gmail.com>
2019-01-04 11:09:40 +0900

    #67 ポリゴンを描けるようにした

--- FootballBoard/Object/ObjectPolygon.cs
+++ FootballBoard/Object/ObjectPolygon.cs
@@ -9,6 +9,20 @@ namespace FootballBoard
         //左クリックしたとき
         public override void LeftMouseDown(Point pos)
         {
+            //クリックしたところにすでに矩形があるか
+            if (this.CurrentObj != null && CurrentObj.CheckDistance(pos))
+            {
+                CurrentObj.ObjStatus = ObjectBase.OBJ_STATUS.DRUG;
+                this.MouseDrag = true;
+            }
+            else
+            {
+                ObjectPolygon poly = new ObjectPolygon(pos);
+                this.model.ObjectList.Add(poly);
+                this.CurrentObj = poly;
+                poly.DrugType = ObjectPolygon.DRUG_TYPE.INIT;
+                CurrentObjIndex = this.model.ObjectList.Count - 1;
+            }
         }
         //左ドラッグ
         public override void MouseMove(Point pos)
@@ -25,7 +39,7 @@ namespace FootballBoard
         {
             this.CurrentObj.ObjStatus = ObjectBase.OBJ_STATUS.SELECT;
         }
-        private ObjectRect CurrentObj;
+        private ObjectPolygon CurrentObj;
     }
     class ObjectPolygon : ObjectBase
     {
@@ -47,15 +61,81 @@ namespace FootballBoard
             POINT_2,
             POINT_3,
             POINT_4,
+            INIT,           //新しく作ったときの動き
         };
         //ドラッグしているときの動き
         public override void DrugMove(Point pos)
         {
-        }
+            //何を掴んでいるかで場合分け
+            switch (this.DrugType)
+            {
+                case DRUG_TYPE.POINT_1:
+                    {
+                        this.Points[0] = pos;
+                    }
+                    break;
+                case DRUG_TYPE.POINT_2:
+                    {
+                        this.Points[1] = pos;
+                    }
+                    break;
+                case DRUG_TYPE.POINT_3:
+                    {
+                        this.Points[2] = pos;
+                    }
+                    break;
+                case DRUG_TYPE.POINT_4:
+                    {
+                        this.Points[3] = pos;
+                    }
+                    break;
+                case DRUG_TYPE.INIT:
+                    {
+                        //新しく作ったときの動き
+                        this.Points[2] = pos;
 
+                        int w = Points[2].X - Points[0].X;
+
+                        this.Points[1].X = this.Points[0].X + (w/2);
+                        this.Points[1].Y = this.Points[0].Y;
+
+                        this.Points[3].X = this.Points[0].X + (w / 2);
+                        this.Points[3].Y = this.Points[2].Y;
+
+
+                    }
+                    break;
+
+                case DRUG_TYPE.WHOLE:
+                    {
+                        //全体を動かす
+                        int move_x = pos.X - this.MoveStartPos.X;
+                        int move_y = pos.Y - this.MoveStartPos.Y;
+
+                        for (int i = 0; i < 4; i++)
+                        {
+                            this.Points[i].X += move_x;
+                            this.Points[i].Y += move_y;
+                        }
+
+                        this.MoveStartPos = pos;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
         //ポリゴンを描画
         public override void DrawObject(Graphics g)
         {
+            if (this.ObjStatus == OBJ_STATUS.NON)
+            {
+                g.FillPolygon(Brushes.Black, this.Points);
+            }
+            else
+            {
+                g.FillPolygon(Brushes.Red, this.Points);
+            }
         }
 
         //オブジェクトとの距離をチェックする
